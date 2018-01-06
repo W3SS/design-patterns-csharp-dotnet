70c1c742e17872b5c29547e4e08ba3cd40d26add
Orestis Meikopoulos <a-ormeik@microsoft.com>
2018-01-06 19:47:38 +0200

    Template Method Pattern

---
+++ TemplateMethodPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TemplateMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chess = new Chess();
+            chess.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
