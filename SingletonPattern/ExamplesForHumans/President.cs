de83774b3a8fd360ad3f7aa84ca87f02aaadc62a
Orestis Meikopoulos <a-ormeik@microsoft.com>
2018-01-08 22:45:53 +0200

    Singleton Design Pattern for Humans + Readme

---
+++ SingletonPattern/ExamplesForHumans/President.cs
@@ -0,0 +1,23 @@
+﻿namespace SingletonPattern.ExamplesForHumans
+{
+    public sealed class President
+    {
+        private static President _instance;
+
+        private President()
+        {
+            // Hide the constructor
+        }
+
+        public static President GetInstance()
+        {
+            if (_instance == null) 
+            {
+                _instance = new President();
+            }
+
+            return _instance;
+        }
+        
+    }
+}
---
+++ SingletonPattern/ExamplesForHumans/SingletonClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonPattern.ExamplesForHumans
+{
+    public class SingletonClient
+    {
+        static void Demo()
+        {
+            var president1 = President.GetInstance();
+            var president2 = President.GetInstance();
+
+            Console.WriteLine(president1 == president2); // true
+            
+            Console.ReadLine();
+        }
+    }
+}
