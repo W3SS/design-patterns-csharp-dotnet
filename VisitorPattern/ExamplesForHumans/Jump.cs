493395016b3f97a0577812aae9fc61d6aadd93d3
Orestis Meikopoulos <a-ormeik@microsoft.com>
2018-01-11 23:37:00 +0200

    Visitor example

---
+++ VisitorPattern/ExamplesForHumans/Jump.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VisitorPattern.ExamplesForHumans
+{
+    public class Jump : IAnimalOperation
+    {
+        public void VisitMonkey(Monkey monkey)
+        {
+            Console.WriteLine("Jumped 20 feet high! on to the tree!");
+        }
+
+        public void VisitLion(Lion lion)
+        {
+            Console.WriteLine("Jumped 7 feet! Back on the ground!");
+        }
+
+        public void VisitDolphin(Dolphin dolphin)
+        {
+            Console.WriteLine("Walked on water a little and disappeared");
+        }
+    }
+}
--- VisitorPattern/ExamplesForHumans/VisitorClient.cs
+++ VisitorPattern/ExamplesForHumans/VisitorClient.cs
@@ -13,8 +13,20 @@ namespace VisitorPattern.ExamplesForHumans
             var speak = new Speak();
 
             monkey.Accept(speak);    // Ooh oo aa aa!    
+
             lion.Accept(speak);      // Roaaar!
+
             dolphin.Accept(speak);   // Tuut tutt tuutt!
+
+            // Another operation
+
+            var jump = new Jump();
+
+            monkey.Accept(jump);    // Jumped 20 feet high! on to the tree!
+
+            lion.Accept(jump);      // Jumped 7 feet! Back on the ground!
+
+            dolphin.Accept(jump);   // Walked on water a little and disappeared
 
             Console.ReadLine();
         }
